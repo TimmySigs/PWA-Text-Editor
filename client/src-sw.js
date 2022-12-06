ed1d0f29741084b41a9da6f90bc50f9f7a955c9a
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-12-06 01:11:39 -0600

    file restructure and building webpack

--- client/src-sw.js
+++ client/src-sw.js
@@ -29,6 +29,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+
   new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
@@ -38,6 +39,4 @@ registerRoute(
       }),
     ],
   })
-
-
 );
